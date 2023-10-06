f069d5bea909fbf2bfbdf0cc1bfb0947c65ae833
FredB <fb6328@gmail.com>
2023-10-06 14:01:01 +0300

    Add

---
+++ EscapeSequence2.cpp
@@ -0,0 +1,20 @@
+/*
+A program for displaying two stick people
+standing next to their car
+Created by FBarasa <FBarasa@strathmore.du>
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Stick Woman and Man Standing next to their Car\n\n";
+    cout << "\t\t ---  \ " << endl;
+    cout << "\t\t(o o)" << endl;
+    cout << "\t\t --\n";
+    cout << "\t\t |" << endl;
+    cout << "\t\t/\\" << endl;
+    cout << "\t\t/\t\\" << endl;
+    cout << "\t|\t|" << endl;
+    return (0);
+}
