738582dd6d10d5a2ad589a81de923657aebdcf1f
FredB <fb6328@gmail.com>
2023-11-09 18:29:06 +0300

    Addedd SuOfValues.cpp and Numbers.cpp

---
+++ Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 12;
+    while (num <= 28) // Loop runs as long as the value of num is <=28
+    {
+        /* code */
+        cout << num;
+        cout << ",";
+        num += 2;
+    }
+}
